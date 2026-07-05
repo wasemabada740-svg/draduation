40abbbf99fb9ba6d5b6b07e731dcd6860597d643
wasemabada740-svg <wasemabada740@gmail.com>
2026-07-06 01:54:49 +0300

    Update urls.py

--- recommender/urls.py
+++ recommender/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import recommend, recommend_by_customer
 
 urlpatterns = [
-    path('recommend/', recommend),
-    path('recommend/customer/', recommend_by_customer),
+    path('recommend', recommend),
+    path('recommend/customer', recommend_by_customer),
 ]
 
