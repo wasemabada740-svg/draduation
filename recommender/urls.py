ab2ffffef7d0ff6348d6e46898513bcaf2fdd9c0
wasemabada740-svg <wasemabada740@gmail.com>
2026-07-06 00:58:37 +0300

    Add files via upload

---
+++ recommender/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import recommend, recommend_by_customer
+
+urlpatterns = [
+    path('recommend/', recommend),
+    path('recommend/customer/', recommend_by_customer),
+]
+
